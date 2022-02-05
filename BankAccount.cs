1db8fc638be42a1417dbecdd826ac58e0d10698e
logan-campbell27 <89537361+logan-campbell27@users.noreply.github.com>
2022-02-04 18:50:42 -0700

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Account_Simulator
+{
+    class BankAccount
+    {
+        private decimal _balance;
+
+        public BankAccount(decimal startingBalance)
+        {
+            _balance = startingBalance;
+        }
+
+        public decimal Balance
+        {
+            get { return _balance; }
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            _balance -= amount;
+        }
+
+
+    }
+}
